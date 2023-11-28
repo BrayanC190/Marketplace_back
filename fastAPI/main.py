08eeb8c3d4e3b0fdad9790ee95afc1dbbd26b953
PZHPedro <jesus505perez@gmail.com>
2023-11-28 15:31:31 -0600

    login y json

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 import json
 #from modelos.usuario import usuario
-from funciones.usuarios import getUser as gu, updateUser as uu, createUser as cu
+from funciones.usuarios import getUser as gu, updateUser as uu, createUser as cu, validateLogin as vl
+from funciones.usuarios import credenciales as lu, updateDatos as ud, newDatos as nd
 from funciones.smallpublicaciones import getSmallPublicaciones as sp
 
 
@@ -16,19 +17,25 @@ async def getUser(nickname:str):
     #usuario_json = json.dumps(usuario_dict)
     return {"prueba"}
 """
+@app.post("/login")
+def login(crendeciales : lu):
+    valid = vl(crendeciales.nickname, crendeciales.password)
+    if (valid): return {"msg":"usuario valido"}
+    else : return {"msg" : "usuario invalido", "user":crendeciales.nickname, "pass":crendeciales.password}
 
 @app.get("/getUser/{nickname}")
 async def getUser(nickname : str):
     usuario = gu(nickname)
     return usuario
 
-@app.put("/updateUser/")
-async def updateUser(nickname : str, telefono : str, calle1 : str, calle2 : str, colonia : str, lote : int, municipio : str, estado : str, pais : str):
-    up = uu(nickname, telefono, calle1, calle2, colonia, lote, municipio, estado, pais)
+@app.put("/updateuser")
+def updateUser(datos : ud):
+    up = uu(datos.nickname, datos.telefono, datos.calle1, datos.calle2, datos.colonia, datos.lote, datos.municipio, datos.estado, datos.pais)
 
-@app.post("/createUser")
-async def createUser(nickname : str, password :str, nombres : str, apellidoP : str, apellidoM :str, fechaN : str, correo : str):
-    new = cu(nickname, password, nombres, apellidoP, apellidoM, fechaN, correo)
+@app.post("/createuser")
+def createUser(cuenta : nd):
+    new = cu(cuenta.nickname, cuenta.password, cuenta.nombres, cuenta.apellidoP, cuenta.apellidoM, cuenta.fechaN, cuenta.correo)
+
 
 @app.get("/getSmallPublicaciones/")
 async def getSmallPublicaciones():
--- funciones/usuarios.py
+++ funciones/usuarios.py
@@ -1,5 +1,46 @@
 from conexion import conexion
 import bcrypt
+from pydantic import BaseModel
+
+class credenciales(BaseModel):
+    nickname: str
+    password: str
+
+class updateDatos(BaseModel):
+    nickname : str
+    telefono : str
+    calle1 : str
+    calle2 : str
+    colonia : str
+    lote : int
+    municipio : str
+    estado : str
+    pais : str
+
+class newDatos(BaseModel):
+    nickname : str
+    password :str
+    nombres : str
+    apellidoP : str
+    apellidoM :str
+    fechaN : str
+    correo : str
+
+def validateLogin(nickname : str, password : str):
+    conn = conexion().getConexion()
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT pass FROM usuarios WHERE nickname = '{nickname}'")
+    fila = cursor.fetchone()
+    pBD = fila[0]
+    password_BD = pBD.encode('utf-8')
+
+    password_hash = hash_password(password)
+
+    is_correct = verify_password(password_BD, password)
+
+    return is_correct
+
+    ##return {"bd": password_BD, "pass":password_hash, "is":is_correct}
 
 def getUser(nickname : str):
     conn = conexion().getConexion()
@@ -44,12 +85,22 @@ def updateUser(nickname : str, telefono : str, calle1 : str, calle2 : str, colon
 def createUser(nickname : str, password :str, nombres : str, apellidoP : str, apellidoM :str, fechaN : str, correo : str):
     conn = conexion().getConexion()
     cursor = conn.cursor()  
-    pass_hash = hash_password(password)
+    pass_hash = hash_password_str(password)
     cursor.execute(f"call newUserBasic('{nickname}', '{pass_hash}', '{nombres}', '{apellidoP}', '{apellidoM}', '{fechaN}', '{correo}')")  
     cursor.close()
     conn.close()
 
-def hash_password(password):
+def hash_password_str(password):
     salt = bcrypt.gensalt()
     pas = bcrypt.hashpw(password.encode('utf-8'), salt)
     return pas.decode('utf-8')
+
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+def verify_password(stored_hash, user_input_password):
+    if isinstance(stored_hash, str):
+        stored_hash = stored_hash.encode('utf-8')
+
+    return bcrypt.checkpw(user_input_password.encode('utf-8'), stored_hash)
