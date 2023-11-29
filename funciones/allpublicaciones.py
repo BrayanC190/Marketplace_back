bcdc661563cc3c926a4ecfd00869caac6039bbeb
Erwin Brow Martinez Herrera <L20021244@veracruz.tecnm.mx>
2023-11-29 15:32:18 -0600

    Update allpublicaciones.py

--- funciones/allpublicaciones.py
+++ funciones/allpublicaciones.py
@@ -1,37 +1,36 @@
 from conexion import conexion
 
-def getAllPublicaciones(idPublicacion : str):
+def getAllPublicaciones(idPublicacion: int):
     conn = conexion().getConexion()
     cursor = conn.cursor()
-    cursor.execute(f"call marketplace.getUser('{idPublicacion}')")
-    fila = cursor.fetchone()
-    #Se crea la lista de publicaciones que contendra (idPublicacion,fecha, Nombre, Precio, Unidad, Descripcion, Nombres, ApellidoP, nickname, Telefono, Correo, Web, Calle1, Calle2, Lote, Colonia, Municipio, Estado, Pais)
+    cursor.execute(f"call marketplace.allPublicacion({idPublicacion})")
+    fila = cursor.fetchall()
     if fila:
         publicacion = {
-            'idPublicacion' : idPublicacion,
-            'fecha' : fila[1],
-            'nombre' : fila[2],
-            'precio' : fila[3],
-            'unidad' : fila[4],
-            'descripcion' : fila[5],
-            'nombres' : fila[6],
-            'apellidoP' : fila[7],
-            'nickname' : fila[8],
-            'telefono' : fila[9],
-            'correo' : fila[10],
-            'web' : fila[11],
-            'calle1' : fila[12],
-            'calle2' : fila[13],
-            'lote' : fila[14],
-            'colonia' : fila[15],
-            'municipio' : fila[16],
-            'estado' : fila[17],
-            'pais' : fila[18],
-        }               
+           'idPublicacion' : idPublicacion,
+            'fecha' : fila[0][1],
+            'nombre' : fila[0][2],
+            'precio' : fila[0][3],
+            'unidad' : fila[0][4],
+            'descripcion' : fila[0][5],
+            'nombres' : fila[0][6],
+            'apellidoP' : fila[0][7],
+            'nickname' : fila[0][8],
+            'telefono' : fila[0][9],
+            'correo' : fila[0][10],
+            'web' : fila[0][11],
+            'calle1' : fila[0][12],
+            'calle2' : fila[0][13],
+            'lote' : fila[0][14],
+            'colonia' : fila[0][15],
+            'municipio' : fila[0][16],
+            'estado' : fila[0][17],
+            'pais' : fila[0][18],
+        }
         cursor.close()
         conn.close()
-        return publicacion 
-    else:         
+        return publicacion
+    else:
         cursor.close()
         conn.close()
         return False
