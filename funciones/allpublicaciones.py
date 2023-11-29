c79996eca08c5fccd21efed204bbef35b13fcbc2
Erwin Brow Martinez Herrera <L20021244@veracruz.tecnm.mx>
2023-11-29 15:34:41 -0600

    Update allpublicaciones.py

--- funciones/allpublicaciones.py
+++ funciones/allpublicaciones.py
@@ -4,28 +4,28 @@ def getAllPublicaciones(idPublicacion: int):
     conn = conexion().getConexion()
     cursor = conn.cursor()
     cursor.execute(f"call marketplace.allPublicacion({idPublicacion})")
-    fila = cursor.fetchall()
+    fila = cursor.fetchone()
     if fila:
         publicacion = {
            'idPublicacion' : idPublicacion,
-            'fecha' : fila[0][1],
-            'nombre' : fila[0][2],
-            'precio' : fila[0][3],
-            'unidad' : fila[0][4],
-            'descripcion' : fila[0][5],
-            'nombres' : fila[0][6],
-            'apellidoP' : fila[0][7],
-            'nickname' : fila[0][8],
-            'telefono' : fila[0][9],
-            'correo' : fila[0][10],
-            'web' : fila[0][11],
-            'calle1' : fila[0][12],
-            'calle2' : fila[0][13],
-            'lote' : fila[0][14],
-            'colonia' : fila[0][15],
-            'municipio' : fila[0][16],
-            'estado' : fila[0][17],
-            'pais' : fila[0][18],
+            'fecha' : fila[1],
+            'nombre' : fila[2],
+            'precio' : fila[3],
+            'unidad' : fila[4],
+            'descripcion' : fila[5],
+            'nombres' : fila[6],
+            'apellidoP' : fila[7],
+            'nickname' : fila[8],
+            'telefono' : fila[9],
+            'correo' : fila[10],
+            'web' : fila[11],
+            'calle1' : fila[12],
+            'calle2' : fila[13],
+            'lote' : fila[14],
+            'colonia' : fila[15],
+            'municipio' : fila[16],
+            'estado' : fila[17],
+            'pais' : fila[18],
         }
         cursor.close()
         conn.close()
